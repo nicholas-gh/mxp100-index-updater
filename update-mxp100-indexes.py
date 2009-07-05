47145c2c27f3310d57ba32870d97c44891b5e499
Nick Piper <nick-git@nickpiper.co.uk>
2009-07-05 10:54:22 +0100

    Add pynotify support

--- update-mxp100-indexes.py
+++ update-mxp100-indexes.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import pynotify
 
 if len(sys.argv) != 2:
     print "Usage: %s PATH_TO_MXP_DISK" % sys.argv[0]
@@ -13,6 +14,11 @@ HDR = "".join(chr(x) for x in [0xbe,0xba,0x02,0x00,0x01,0x00] + [0x0] * 318)
 BLOCK_LEN = 716
 
 REDUNDANT_FILES=False
+
+pynotify.init("e.Digital MXP100")
+notify_start = pynotify.Notification("e.Digital MXP100 Sync", "Indexing started.", "gtk.STOCK_INFO")
+notify_start.set_urgency(pynotify.URGENCY_LOW)
+notify_start.show()
 
 def has_music_children(path):
     for dir, dirs, files in os.walk(path):
@@ -51,4 +57,9 @@ for dir, dirs, files in os.walk(ROOT):
             playlist.write("".join(chr(x) for x in padding))
         playlist.close()
         os.chmod(playlist_fn, 0755)
-        
+
+notify_start.close()
+n = pynotify.Notification("e.Digital MXP100 Sync", "Index update completed.", "gtk.STOCK_INFO")
+n.set_urgency(pynotify.URGENCY_NORMAL)
+#n.set_timeout(pynotify.EXPIRES_NEVER)
+n.show()
